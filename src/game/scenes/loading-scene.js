2be5914b451cc7087ef8f0735b79f1f580739153
gianna gartmann <e254623@sbl.ch>
2025-02-07 15:40:56 +0000

    Spielerdesign erstellt

--- src/game/scenes/loading-scene.js
+++ src/game/scenes/loading-scene.js
@@ -49,6 +49,7 @@ export default class LoadingScene extends Phaser.Scene {
    * die Lade-Szene brauchen wir aber nur einen Text.
    */
   create() {
+    this.createAnimations()
     // Damit erstellen wir ein Spielobjekt Text. Wir geben die Position in x und y
     // an, und geben den Text der angezeigt werden soll an.
     const text = this.add.text(320, 240, "Press SPACE to start the Game.")
@@ -70,7 +71,37 @@ export default class LoadingScene extends Phaser.Scene {
       // Die Leertaste wurde gedrückt, jetzt möchten wir eine neue Szene laden.
       // Das was wir hier übergeben, ist der Schlüssel/Name der Szene, so wie
       // es im Konstruktor angegeben wurde.
-      this.scene.start("level-00")
+      this.scene.start("level-01")
     }
+  }
+
+  // Die Funktion packt uns schön alles zusammen was mit dem erstellen von
+  // Animationen zu tun hat.
+  createAnimations() {
+    // Das erstellt uns eine Animation. Hier können wir mehrere Parameter übergeben
+    // um die Animation zu definieren.
+    this.anims.create({
+      key: "player_idle", // Das ist der Name der Animation, den brauchen wir um die Animation abzuspielen
+      frames: this.anims.generateFrameNumbers("player", {
+        // Das übernimmt das eigentlich erstellen der Animationsframes. Hier geben wir an von welchem Spritesheet die Animation erstellt wird. Das Spritesheet muss natürlich auch in der `preload`-Methode geladen werden.
+        start: 1, // Bei welcher Kachel die Animation beginnt.
+        end: 1, // Bei welcher Kachel die Animation fertig ist.
+      }),
+      frameRate: 10, // Mit welcher Geschwindigkeit die Animation abläuft. Spielt hier keine Rolle, denn wir haben nur 1 Frame
+      repeat: -1, // Wie oft die Animation wiederholt wird. Mit -1 läuft sie in einer Dauerschleife.
+    })
+
+    // Hier wird die Animation für das Rechtslaufen erstellt.
+    this.anims.create({
+      key: "player_right",
+      frames: this.anims.generateFrameNumbers("player", {
+        start: 6,
+        end: 8,
+      }),
+      frameRate: 10,
+      repeat: -1,
+    })
+
+    // TODO: Erstellen Sie die restlichen Animationen
   }
 }
